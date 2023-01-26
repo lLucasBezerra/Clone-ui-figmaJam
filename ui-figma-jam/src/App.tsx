9799f9d185f7e87860a4852a804b51a271c13858
José Lucas <bezerra00714@hotmail.com>
2023-01-26 20:22:04 -0300

    projeto criado

---
+++ ui-figma-jam/src/App.tsx
@@ -0,0 +1,25 @@
+import ReactFlow, { Background, Controls } from "reactflow";
+import "reactflow/dist/style.css";
+import { zinc } from "tailwindcss/colors";
+import "./global.css";
+
+function App() {
+
+  return (
+    <div 
+      className="w-screen h-screen"
+    >
+      <ReactFlow>
+        <Background 
+          gap={12}
+          size={2}
+          color={zinc[200]}
+        />
+        <Controls />
+      </ReactFlow>
+
+    </div>
+  )
+}
+
+export default App
