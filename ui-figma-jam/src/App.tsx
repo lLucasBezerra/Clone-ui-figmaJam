975847530570f24d7d0ee9a74023687eccf853b1
José Lucas <bezerra00714@hotmail.com>
2023-02-01 20:50:15 -0300

    Square node criado e programado para mover

--- ui-figma-jam/src/App.tsx
+++ ui-figma-jam/src/App.tsx
@@ -1,15 +1,67 @@
-import ReactFlow, { Background, Controls } from "reactflow";
+import { useCallback } from "react";
+import ReactFlow, 
+{ addEdge,
+  Background, 
+  Connection, 
+  ConnectionMode, 
+  Controls, 
+  Node, 
+  useEdgesState,
+  useNodesState
+} from "reactflow";
+
 import "reactflow/dist/style.css";
 import { zinc } from "tailwindcss/colors";
+import { Square } from "./components/nodes/Square";
 import "./global.css";
 
-function App() {
+const NODE_TYPES = {
+  square: Square
+};
+const INITIAL_NODES = 
+[
+   {
+    id: crypto.randomUUID(),
+    type: "square",
+    position: {
+      x: 200,
+      y: 400,
+    },
+    data: {},
+   },
+   {
+    id: crypto.randomUUID(),
+    type: "square",
+    position: {
+      x: 600,
+      y: 400,
+    },
+    data: {},
+   },
 
+] satisfies Node[]
+
+
+function App() {
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [nodes, setNodes, onNodesChange] = useNodesState(INITIAL_NODES);
+
+  const onConnect = useCallback((connection: Connection) => {
+    return setEdges(edges => addEdge(connection, edges));
+  }, []);
   return (
     <div 
       className="w-screen h-screen"
     >
-      <ReactFlow>
+      <ReactFlow
+        nodeTypes={NODE_TYPES}
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        connectionMode={ConnectionMode.Loose}
+      >
         <Background 
           gap={12}
           size={2}
